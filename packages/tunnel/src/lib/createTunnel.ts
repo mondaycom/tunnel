ea9c2e38e299aee53053f78c8675a2bfb264b501
Adam Rutkowski <adamru@monday.com>
2022-08-18 15:01:22 +0200

    refactor: remove unused type

--- packages/tunnel/src/lib/createTunnel.ts
+++ packages/tunnel/src/lib/createTunnel.ts
@@ -1,11 +1,6 @@
 import { TunnelConnection } from './TunnelConnection';
 import { TunnelOptions } from './types';
 
-export type TunnelCallback = (
-  err: Error | null,
-  connection: TunnelConnection | null
-) => void;
-
 async function createTunnel(options: TunnelOptions): Promise<TunnelConnection> {
   const client = new TunnelConnection(options);
   await client.open();
